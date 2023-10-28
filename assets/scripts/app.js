53d95fbe099720d824db5bdfb8b53137a10ae31a
Arnaud BODEL <arnaud.bodel@gmail.com>
2023-10-28 18:30:19 +0200

    -

---
+++ assets/scripts/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import css from "./../styles/styles.css";
+
+console.log("Hello There");
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,20 @@ const INPUT = 'assets/scripts/app.js';
 const OUTPUT = 'public/bundle/';
 
 module.exports = {
-    entry: path.resolve(__dirname, INPUT),
-    output: {
-      path: path.resolve(__dirname, OUTPUT),
-      filename: 'bundle.js',
-    },
+  entry: path.resolve(__dirname, INPUT),
+  output: {
+    path: path.resolve(__dirname, OUTPUT),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
 };
   
