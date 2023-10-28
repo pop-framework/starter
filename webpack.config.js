09badbc7e53207b8df8ffe63755703f91ff8400e
Arnaud BODEL <arnaud.bodel@gmail.com>
2023-10-28 18:55:14 +0200

    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const path = require('path');
 
 const INPUT = 'assets/scripts/main.js';
-const OUTPUT = 'public/bundle/';
+
+// Output
+const OUTPUT_DIRECTORY = 'public/';
+const OUTPUT_FILE = 'bundle.js';
+
+
 
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, INPUT),
   output: {
-    path: path.resolve(__dirname, OUTPUT),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, OUTPUT_DIRECTORY),
+    filename: OUTPUT_FILE,
   },
 
   module: {
